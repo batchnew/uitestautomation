db1ce1b145aaba1be9d8cc28f35630220836da62
umesh2997 <umeshpamu9@gmail.com>
2024-04-28 13:59:02 +0530

    register

---
+++ TestAutomationFrameWork/src/test/java/com/constructer/Register.java
@@ -0,0 +1,10 @@
+package com.constructer;
+
+public class Register {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("I am register class");
+	}
+
+}
